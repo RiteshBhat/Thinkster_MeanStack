fadb048f12a835188e2a385958576a967169f446
Ritesh Bhat <riteshbhat.92@gmail.com>
2017-03-15 00:40:57 +0530

    Loading Posts

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,77 @@
+angular.module('flapperNews', ['ui.router'])
+.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl',
+			resolve: {
+				postPromise: ['posts', function(posts){
+					return posts.getAll();
+				}]
+			}
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+		$urlRouterProvider.otherwise('home');
+
+	}])
+.factory('posts', ['$http', function($http){
+			//service body
+			var o = {
+				posts: []
+			};
+			o.getAll = function() {
+				return $http.get('/posts').success(function(data){
+					angular.copy(data, o.posts);
+				});
+			};
+			return o;
+		}])
+.controller('MainCtrl', [
+	'$scope',
+	'posts',
+	function($scope, posts){
+		$scope.posts = posts.posts;
+		$scope.addPost = function() {
+			if(!$scope.title || $scope.title === ' ') {return;}
+			$scope.posts.push({
+				title: $scope.title, 
+				link: $scope.link,
+				upvotes: 0,
+				comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
+			});
+		};
+
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes += 1;
+		};
+	}])
+.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		$scope.posts = posts.posts[$stateParams.id];
+
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
+		};
+	}]);
